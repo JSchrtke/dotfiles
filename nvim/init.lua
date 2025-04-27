fd43e61282649752b70f44c048e484de87027a6e
jschrtke <jschrtke@proton.me>
2025-04-27 21:48:40 +0200

    Update nvim options & configure lua lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,53 @@
 vim.o.number=true
 vim.o.relativenumber=true
+vim.o.grepprg="rg --vimgrep"
+vim.o.clipboard="unnamedplus"
+vim.o.listchars = "tab:-->,eol:↲,nbsp:␣,space:•,trail:•,extends:⟩,precedes:⟨"
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.cursorline = true
+vim.o.colorcolumn = "80,100,120"
+vim.o.signcolumn = "yes"
+
+-- keymaps
+local map = vim.api.nvim_set_keymap
+-- quickfix list
+map('n', '<C-n>', ':cnext<cr>', {})
+map('n', '<C-p>', ':cprev<cr>', {})
+-- diagnostic
+map("n", "sd", ":lua vim.diagnostic.open_float()<CR>", {})
+map("n", "sad", ":lua vim.diagnostic.setqflist({ title = 'Diagnostics' })<CR>", {})
+map("n", "se", ":lua vim.diagnostic.setqflist({ title = 'Errors', severity = vim.diagnostic.severity.ERROR })<CR>", {})
+map("n", "]e", ":lua vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })<CR>", {})
+map("n", "[e", ":lua vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })<CR>", {})
+
+
+-- lsp config
+vim.lsp.config['lua'] = {
+    cmd = { 'lua-language-server' },
+    filetypes = { 'lua' },
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        },
+    },
+    root_markers = {
+        '.luarc.json',
+        '.luarc.jsonc',
+        '.luacheckrc',
+        '.stylua.toml',
+        'stylua.toml',
+        'selene.toml',
+        'selene.yml',
+        '.git',
+    },
+}
+vim.lsp.enable('lua')
 
 vim.cmd([[
 " Wez's VIM colors for Dark background
